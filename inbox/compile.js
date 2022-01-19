4a34401e6d018fe29d3599721fe7b22030432a71
Colin MacLeod <colinlmacleod1@gmail.com>
2022-01-18 22:47:06 -0500

    added compile script

--- inbox/compile.js
+++ inbox/compile.js
@@ -1,1 +1,16 @@
-// compile code will go here
+const path = require("path");
+const solc = require("solc");
+const fs = require("fs-extra");
+
+// get path to build folder
+const buildPath = path.resolve(__dirname, "build");
+// delete build folder
+fs.removeSync(buildPath);
+ 
+// get path to Campaigns.sol
+const campaignPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+// read campaign file
+const source = fs.readFileSync(campaignPath, "utf8");
+// compile contracts and get contracts
+
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
